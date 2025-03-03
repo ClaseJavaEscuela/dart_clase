7afb1e7c2cc90791a62c4f57e4990731751d0641
= <luis019dell@gmail.com>
2025-03-03 15:09:21 -0600

    primer commit

---
+++ primerosPasos/conceptos.dart
@@ -0,0 +1,21 @@
+bool esMayorDeEdad(int edad){
+  
+  if(edad<18){
+    return false;
+  }
+  return true;
+}
+
+void main() {
+  String nombre = "luis";
+  int edad = 15;
+  double peso = 80.25;
+  
+  //NullSafety: maneja los valores nulos
+  // colocando un ? le indicamos ala variable que puede llegar un momento en el que se le asigne un valor nulo
+  bool? esMayor = false;
+  
+  //litas
+  List<int> numero = [];
+  
+}
